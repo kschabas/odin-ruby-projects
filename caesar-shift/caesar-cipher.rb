5282c9435b3e6bb9ab393da55ae7844a740013a8
Karl Schabas <karl.schabas@utoronto.ca>
2023-02-02 11:54:34 -0500

    Complete cipher project

--- caesar-shift/caesar-cipher.rb
+++ caesar-shift/caesar-cipher.rb
@@ -1,2 +1,20 @@
-def caesar_cipher(str)
+def caesar_cipher(str,shift)
+    result = ""
+    str.each_char do |char| 
+       if char.ord >='a'.ord && char.ord <='z'.ord 
+            next_char = (char.ord + shift).chr
+            if next_char.ord > 'z'.ord
+                next_char = (next_char.ord - 'z'.ord + 'a'.ord - 1).chr
+            end 
+       elsif char.ord >= 'A'.ord && char.ord <='Z'.ord
+            next_char = (char.ord + shift).chr
+            if next_char.ord > 'Z'.ord
+                next_char = (next_char.ord-'Z'.ord + 'A'.ord - 1).chr
+            end
+        else
+            next_char = char;
+       end
+        result += next_char
+    end
+    result
 end
