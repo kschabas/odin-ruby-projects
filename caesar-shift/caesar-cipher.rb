76a4dde80d467081e2ee0897d85920e91a9e401f
Karl Schabas <karl.schabas@utoronto.ca>
2023-02-02 13:47:50 -0500

    Add between? function for readability

--- caesar-shift/caesar-cipher.rb
+++ caesar-shift/caesar-cipher.rb
@@ -1,12 +1,12 @@
 def caesar_cipher(str,shift)
     result = ""
     str.each_char do |char| 
-       if char.ord >='a'.ord && char.ord <='z'.ord 
+       if char.ord.between?('a'.ord,'z'.ord)
             next_char = (char.ord + shift).chr
             if next_char.ord > 'z'.ord
                 next_char = (next_char.ord - 'z'.ord + 'a'.ord - 1).chr
             end 
-       elsif char.ord >= 'A'.ord && char.ord <='Z'.ord
+       elsif char.ord.between?('A'.ord,'Z'.ord)
             next_char = (char.ord + shift).chr
             if next_char.ord > 'Z'.ord
                 next_char = (next_char.ord-'Z'.ord + 'A'.ord - 1).chr
