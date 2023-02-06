8f1d7b67e6bb71b7e2320c228a249040c51ae242
Karl Schabas <karl.schabas@utoronto.ca>
2023-02-06 18:05:05 -0500

    Built print_board functionality

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -1,5 +1,33 @@
-class Board 
-    @board = Array.new(3,Array.new(3))
+# frozen_string_literal: false
 
-    def intialize
+# Tic tac toe board class
+class Board
+  def initialize
+    @board = Array.new(3, Array.new(3, ''))
+    @turn = 1
+  end
+
+  def clear_board
+    @board.each_index { |index| @board[index] = [' ', ' ', ' '] }
+  end
+
+  def print_board
+    @board.each_index do |index|
+      print_line(@board[index])
+      print_line('-') unless index == 2
+    end
+  end
+
+  private
+
+  def print_line(line)
+    if line == '-'
+      puts '---------'
+    else
+      puts " #{line[0]} | #{line[1]} | #{line[2]} "
+    end
+  end
 end
+
+test = Board.new
+test.print_board
