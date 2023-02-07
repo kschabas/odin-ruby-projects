0c8a96fb2a03b89b630c183d0304f2f76fe111ec
Karl Schabas <karl.schabas@utoronto.ca>
2023-02-06 20:13:27 -0500

    Add move entering functionality

--- tic-tac-toe/tic_tac_toe.rb
+++ tic-tac-toe/tic_tac_toe.rb
@@ -1,10 +1,30 @@
 # frozen_string_literal: false
 
+# class to take care of game
+class Game
+  def initialize
+    @board = Board.new
+    @turn = 1
+  end
+
+  def play_game
+    @board.clear
+    until @board.winner?
+      (coord = get_input(@turn)) until @board.set_move(coord.split(','), @turn)
+      @turn = (@turn + 1) % 2
+    end
+  end
+
+  def get_input
+    print "Player #{@turn}: Please enter move as x,y"
+    gets.chomp
+  end
+end
+
 # Tic tac toe board class
 class Board
   def initialize
-    @board = Array.new(3, Array.new(3, ''))
-    @turn = 1
+    @board = Array.new(3, Array.new(3, ' '))
   end
 
   def clear_board
@@ -16,6 +36,14 @@ class Board
       print_line(@board[index])
       print_line('-') unless index == 2
     end
+  end
+
+  def set_move(coord, turn)
+    return false if coord.length != 2 || !coord.between(0, 2) || !coord.between(0, 2)
+
+    char = turn == 1 ? 'X' : 'O'
+    @board[coord[0], coord[1]] = char
+    true
   end
 
   private
